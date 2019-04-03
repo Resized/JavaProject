d5b67e8da0af318f37a902394b05b2f8db421435
Resized <shai.yarden@gmail.com>
2019-04-03 03:59:09 +0300

    Implemented basic component adding on runtime

--- src/AddComponentOnJFrameAtRuntime.java
+++ src/AddComponentOnJFrameAtRuntime.java
@@ -9,24 +9,34 @@ import java.awt.BorderLayout;
 public class AddComponentOnJFrameAtRuntime extends JFrame implements ActionListener {
 
     private JPanel panel;
+    private JPanel addPanel;
+    private String[] petStrings = {"Bird", "Cat", "Dog", "Rabbit", "Pig"};
+    private String[] choice = {"Procedures", "Inventory", "Lens to transplant"};
+    private JComboBox choiceCombo = new JComboBox<>(choice);
+    private JButton button = new JButton("Add");
+
 
     public AddComponentOnJFrameAtRuntime() {
         super("Add component on JFrame at runtime");
         setLayout(new BorderLayout());
         this.panel = new JPanel();
+        this.addPanel = new JPanel();
         this.panel.setLayout(new FlowLayout());
+        this.addPanel.setLayout(new FlowLayout());
         add(panel, BorderLayout.CENTER);
-        JButton button = new JButton("CLICK HERE");
-        add(button, BorderLayout.SOUTH);
+        addPanel.add(choiceCombo, BorderLayout.SOUTH);
+        addPanel.add(button, BorderLayout.SOUTH);
         button.addActionListener(this);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(addPanel, BorderLayout.SOUTH);
         setSize(500, 500);
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent evt) {
-        this.panel.add(new JButton("Button"));
-        this.panel.add(new JComboBox<>());
+
+        this.choiceCombo.getSelectedItem();
+        this.panel.add(new JComboBox<>(petStrings));
         this.panel.revalidate();
         validate();
     }
